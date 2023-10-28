6eb39347876eb146418ecb29b3da30b25e78f138
ali7arb <138344082+ali7arb@users.noreply.github.com>
2023-10-28 12:59:33 +0300

    added

--- encapsulationTask.dart
+++ encapsulationTask.dart
@@ -1,23 +1,5 @@
-class Employee {
-  int? _id;
-  String? _name;
-
-  int getId() {
-    return _id!;
-  }
-
-  String getName() {
-    return _name!;
-  }
-
-  void setId(int id) {
-    this._id = id;
-  }
-
-  void setName(String name) {
-    this._name = name;
-  }
-}
+import 'empClassEnc.dart';
+//This file for Encapsulation
 
 void main() {
   Employee emp = Employee();
